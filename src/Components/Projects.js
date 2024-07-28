c5fbca2d518c3974163e21f72ae694a689b4a14a
Vidusha620 <hasindividusha@gmail.com>
2024-07-28 17:29:43 +0530

    Update Projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -35,27 +35,23 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>So far from what I've learned and experienced,here is what I bring to the table.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Software Development</h5>
+                                <h5>Web Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Web Designing and Development</h5>
+                                <h5>Brand Identity</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Backend Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Machine Learning</h5>
+                                <h5>Logo Design</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>UI/UX Designing</h5>
+                                <h5>Web Development</h5>
                             </div>
                         </Carousel>
                     </div>
