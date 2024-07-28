7fa2edc6ed71a39c45213f62d00c7edfb0289213
Vidusha620 <hasindividusha@gmail.com>
2024-07-28 14:55:51 +0530

    Update Banner's assets

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Tech Enthusiast", "Software Developer", "Full-stack Developer" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hello! I'm Vidusha,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Tech Enthusiast", "Software Developer", "Full-stack Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>I'm a passionate Computer Science Undergraduate,willing to explore each and every aspects of wonderful tech world.I'm eager to learn and find interesting solutions through unique ideas and gain valuable experiences through this journey.Let’s work together to create a fascinating future!</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
